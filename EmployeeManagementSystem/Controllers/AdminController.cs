32c1eab4c25611e8d4d4633e52a74ee945c432cf
Ashish Sapkota <aacsspkt3@gmail.com>
2021-06-29 00:37:54 +0545

    A very minimal admin dashboard added

---
+++ EmployeeManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagementSystem.Foundations;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUnitOfWork unitWork;
+
+        public AdminController(IUnitOfWork unitOfWork)
+        {
+            this.unitWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+             var departments = await unitWork.Departments.GetAllAsync();
+            var designations = await unitWork.Designations.GetAllAsync();
+            var employees = await unitWork.Employees.GetAllAsync();
+            var vacancies = await unitWork.Vacancies.GetAllAsync();
+            var leaves = await unitWork.Leaves.GetAllAsync();
+            var users = await unitWork.Users.GetAllAsync();
+            ViewData["Departments"] = departments.Count();
+            ViewData["Designations"] = designations.Count();
+            ViewData["Employees"] = employees.Count();
+            ViewData["Vacancy"] = vacancies.Count();
+            ViewData["Leaves"] = leaves.Count();
+            ViewData["Users"] = users.Count();
+            return View();
+        }
+    }
+}
